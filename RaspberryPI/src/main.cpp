689449769f0d064b1dc505e41fdf217674098f75
jgentile <genti337@gmail.com>
2025-06-03 21:08:08 -0500

    Adding threading

--- RaspberryPI/src/main.cpp
+++ RaspberryPI/src/main.cpp
@@ -3,22 +3,60 @@
 #include <json-c/json.h>
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <atomic>
+
+void fetch_loop(std::atomic<bool>& running) {
+    int frame = 0;
+
+    while (running) {
+        FetchData fetcher("https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard");
+        std::string data = fetcher.fetch();
+
+        ESPNParser parser;
+
+        if (!data.empty()) {
+            std::vector<Competition> competitions;
+            competitions = parser.parseESPNScoreboard(data);
+
+            std::cout << "Length of competitions: " << competitions.size() << std::endl;
+
+        } else {
+            std::cerr << "No data received.\n";
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));  // Fast update
+    }
+
+    return;
+}
+
+void display_loop(std::atomic<bool>& running) {
+
+    while (running) {
+        printf("Updating Display\n");
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));  // Fast update
+    }
+
+    return;
+}
 
 int main() {
-    FetchData fetcher("https://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard");
-    std::string data = fetcher.fetch();
+    std::atomic<bool> running(true);
 
-    ESPNParser parser;
 
-    if (!data.empty()) {
-        std::vector<Competition> competitions;
-        competitions = parser.parseESPNScoreboard(data);
+    std::thread displayThread(display_loop, std::ref(running));
+    std::thread fetchThread(fetch_loop, std::ref(running));
 
-        std::cout << "Length of competitions: " << competitions.size() << std::endl;
+    std::cout << "Press Enter to stop..." << std::endl;
+    std::cin.get();  // Wait for user input
 
-    } else {
-        std::cerr << "No data received.\n";
-    }
+    running = false;
+
+    displayThread.join();
+    fetchThread.join();
+
+    std::cout << "All threads stopped." << std::endl;
 
     return 0;
 }
