aef6e7948a35f897e0a16717febe38e1f851e2c5
genti337 <genti337@gmail.com>
2025-07-27 08:11:28 -0500

    Cleaning up CountDown display to use Display base class

--- RaspberryPI/include/CountdownDisplay.hh
+++ RaspberryPI/include/CountdownDisplay.hh
@@ -12,6 +12,8 @@
 #include "graphics.h"
 #include <Magick++.h>
 
+#include "Display.hh"
+
 struct Countdown {
     int days;
     int hours;
@@ -19,45 +21,28 @@ struct Countdown {
     int seconds;
 };
 
-class CountdownDisplay {
+class CountdownDisplay : public Display {
 public:
     CountdownDisplay(int rows, int cols, int chain_length, const std::string& hardware_mapping, bool active);
     ~CountdownDisplay();
 
-    void setText(const std::string& text);
-    void setColor(uint8_t r, uint8_t g, uint8_t b);
-    int getTextWidth(const rgb_matrix::Font& font, const std::string& text);
-    void center_text(const rgb_matrix::Font& font, const std::string& text, int min_x, int max_x, int y, int red=255, int green=255, int blue=255);
-    void center_text(const rgb_matrix::Font& font, const std::string& text, int min_x, int max_x, int y, rgb_matrix::Color color);
-    void draw_text(const rgb_matrix::Font& font, const std::string& text, int x, int y, rgb_matrix::Color color);
-    void drawImage(const std::string& path, int offset_x = 0, int offset_y = 0);
-    rgb_matrix::Color colorFromHex(const std::string& hex);
     void DrawCanvas(rgb_matrix::FrameCanvas* src, rgb_matrix::FrameCanvas* dst, int offset_x, int offset_y);
     Countdown time_until(int month, int day, int hour, int minute);
     void render(int month, int day, int hour, int minute, std::string event);
     void set_sport(std::string ext_sport, std::string ext_league, std::string ext_team);
-    void loadFont(const std::string& font_path);
 
 private:
-    rgb_matrix::RGBMatrix* matrix;
-    rgb_matrix::FrameCanvas* canvas;
-    rgb_matrix::Font font;
-    rgb_matrix::Font temp_font;
-    rgb_matrix::Font score_font;
+    rgb_matrix::Font countdown_font;
     rgb_matrix::Font small_font;
-    rgb_matrix::Color textColor;
-    rgb_matrix::Color bg_color;
-    std::string currentText;
-    std::string sport;
-    std::string league;
-    std::string team;
 
     int max_display_x;
-    bool first_pass;
 
     Countdown time_until_event;
 
     std::map<std::string, std::string> images_map;
+    std::string sport;
+    std::string league;
+    std::string team;
 };
 
 #endif
--- RaspberryPI/src/CountdownDisplay.cpp
+++ RaspberryPI/src/CountdownDisplay.cpp
@@ -5,36 +5,12 @@
 using namespace rgb_matrix;
 using namespace Magick;
 
-CountdownDisplay::CountdownDisplay(int rows, int cols, int chain_length, const std::string& hardware_mapping, bool active) {
-    if (!active) return;
+CountdownDisplay::CountdownDisplay(int rows, int cols, int chain_length, const std::string& hardware_mapping, bool active) :
+    Display(rows, cols, chain_length, hardware_mapping, active) {
 
-    RGBMatrix::Options options;
-    options.rows = rows;
-    options.cols = cols;
-    options.chain_length = chain_length;
-    options.parallel = 1;
-    options.hardware_mapping = hardware_mapping.c_str();
-    options.pwm_bits = 11; //8;
-    options.pwm_lsb_nanoseconds = 200; //180; //130;  // ✅ Fine for Pi 4 or Zero 2 W
-    options.brightness = 75; //50;  // ✅ Fine for Pi 4 or Zero 2 W
-
-    RuntimeOptions runtime_opt;
-    runtime_opt.gpio_slowdown = 5;
-    matrix = CreateMatrixFromOptions(options, runtime_opt);
-    canvas = matrix->CreateFrameCanvas();
-
-    //FIXME loadFont("../rpi-rgb-led-matrix/fonts/6x10.bdf");  // Adjust to your font path
-    font.LoadFont("../rpi-rgb-led-matrix/fonts/6x10.bdf");
-    temp_font.LoadFont("../rpi-rgb-led-matrix/fonts/9x18B.bdf");
-    score_font.LoadFont("../rpi-rgb-led-matrix/fonts/7x14B.bdf");
+    // Load Countdown Timer Specific Font
+    countdown_font.LoadFont("../rpi-rgb-led-matrix/fonts/7x14B.bdf");
     small_font.LoadFont("../rpi-rgb-led-matrix/fonts/5x7.bdf");
-
-    textColor = rgb_matrix::Color(255, 255, 255);  // Default: white
-    bg_color = rgb_matrix::Color(0, 0, 0);  // Default: black
-
-    InitializeMagick(nullptr);
-
-    first_pass = true;
 
     // Images Map
     images_map["CampingLeft"] = "../images/tent.bmp";
@@ -51,103 +27,12 @@ CountdownDisplay::~CountdownDisplay() {
     delete matrix;
 }
 
+void CountdownDisplay::set_sport(std::string ext_sport, std::string ext_league, std::string ext_team) {
+    sport = ext_sport;
+    league = ext_league;
+    team = ext_team;
 
-void CountdownDisplay::loadFont(const std::string& font_path) {
-    if (!font.LoadFont(font_path.c_str())) {
-        std::cerr << "Couldn't load font: " << font_path << std::endl;
-        exit(1);
-    }
-}
-
-void CountdownDisplay::setText(const std::string& text) {
-    currentText = text;
-}
-
-void CountdownDisplay::setColor(uint8_t r, uint8_t g, uint8_t b) {
-    textColor = rgb_matrix::Color(r, g, b);
-}
-
-int CountdownDisplay::getTextWidth(const rgb_matrix::Font& font, const std::string& text) {
-    int width = 0;
-    for (char c : text) {
-        width += font.CharacterWidth(c);
-    }
-    return width;
-}
-
-void CountdownDisplay::center_text(const rgb_matrix::Font& font, const std::string& text,
-                          int min_x, int max_x, int y,
-                          int red, int green, int blue) {
-    int text_width = getTextWidth(font, text);
-    int x = min_x + (max_x - min_x - text_width) / 2;
-    DrawText(canvas, font, x, y, rgb_matrix::Color(red, green, blue), nullptr, text.c_str());
-
-    max_display_x = std::max(max_display_x, max_x);
-}
-
-void CountdownDisplay::center_text(const rgb_matrix::Font& font, const std::string& text,
-                          int min_x, int max_x, int y,
-                          rgb_matrix::Color color) {
-    int text_width = getTextWidth(font, text);
-    int x = min_x + (max_x - min_x - text_width) / 2;
-    DrawText(canvas, font, x, y, color, nullptr, text.c_str());
-
-    max_display_x = std::max(max_display_x, max_x);
-}
-
-void CountdownDisplay::draw_text(const rgb_matrix::Font& font, const std::string& text,
-                               int x, int y, rgb_matrix::Color color) {
-    DrawText(canvas, font, x, y, color, nullptr, text.c_str());
-
-    max_display_x = std::max(max_display_x, x + getTextWidth(font, text));
-}
-
-void CountdownDisplay::drawImage(const std::string& path, int offset_x, int offset_y) {
-    Magick::Image image;
-
-    try {
-       image.read(path);
-    } catch (const Magick::Exception &error) {
-       std::cerr << "Failed to read image " << path << ": " << error.what() << std::endl;
-       return;
-    }
-
-    image.type(Magick::TrueColorType);
-//    image.flip();              // Optional: Flip vertically
-    image.modifyImage();       // Allow pixel access
-
-    for (size_t y = 0; y < image.rows(); ++y) {
-        for (size_t x = 0; x < image.columns(); ++x) {
-            const Magick::ColorRGB color = image.pixelColor(x, y);
-
-            int draw_x = static_cast<int>(x) + offset_x;
-            int draw_y = static_cast<int>(y) + offset_y;
-
-            // Optional: skip out-of-bounds pixels
-            if (draw_x >= 0 && draw_x < canvas->width() &&
-                draw_y >= 0 && draw_y < canvas->height()) {
-                canvas->SetPixel(draw_x, draw_y,
-                                 static_cast<uint8_t>(color.red() * 255),
-                                 static_cast<uint8_t>(color.green() * 255),
-                                 static_cast<uint8_t>(color.blue() * 255));
-            }
-        }
-    }
-
-    max_display_x = std::max(max_display_x, offset_x + int(image.columns()));
-}
-
-rgb_matrix::Color CountdownDisplay::colorFromHex(const std::string& hex) {
-    std::string cleaned = hex[0] == '#' ? hex.substr(1) : hex;
-
-    if (cleaned.length() != 6)
-        throw std::invalid_argument("Hex string must be 6 characters");
-
-    int r = std::stoi(cleaned.substr(0, 2), nullptr, 16);
-    int g = std::stoi(cleaned.substr(2, 2), nullptr, 16);
-    int b = std::stoi(cleaned.substr(4, 2), nullptr, 16);
-
-    return rgb_matrix::Color(r, g, b);
+    return;
 }
 
 Countdown CountdownDisplay::time_until(int month, int day, int hour, int minute) {
@@ -186,14 +71,6 @@ Countdown CountdownDisplay::time_until(int month, int day, int hour, int minute)
     return countdown;
 }
 
-void CountdownDisplay::set_sport(std::string ext_sport, std::string ext_league, std::string ext_team) {
-    sport = ext_sport;
-    league = ext_league;
-    team = ext_team;
-
-    return;
-}
-
 void CountdownDisplay::render(int month, int day, int hour, int minute, std::string event) {
     // Rest Maximum Display X
     max_display_x = 0;
@@ -227,12 +104,13 @@ void CountdownDisplay::render(int month, int day, int hour, int minute, std::str
     std::ostringstream oss2("");
     oss2 << " until " << event << "!";
 
-    //center_text(score_font, oss.str(), 0, canvas->width(), 16, rgb_matrix::Color(255, 255, 255));
-    center_text(score_font, oss.str(), 32, 288, 12, rgb_matrix::Color(255, 255, 255));
-    center_text(score_font, oss2.str(), 32, 288, 28, rgb_matrix::Color(255, 255, 255));
+    //center_text(countdown_font, oss.str(), 0, canvas->width(), 16, rgb_matrix::Color(255, 255, 255));
+    center_text(countdown_font, oss.str(), 32, 288, 12, rgb_matrix::Color(255, 255, 255));
+    center_text(countdown_font, oss2.str(), 32, 288, 28, rgb_matrix::Color(255, 255, 255));
 
     // Update the Canvas
     canvas = matrix->SwapOnVSync(canvas);
+
 
     return;
 }
